927e271c9bcd783f7631f76a662edf54d317b8df
tom426 <tom426@yeah.net>
2025-04-02 17:06:11 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -12,14 +12,14 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 自定义软件源
-git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
+#git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
 # Mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+#find ./ | grep Makefile | grep mosdns | xargs rm -f
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
+g#it clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
